c915be1dc258633610b452a10970822ff79ae671
m1ga <migamiga@web.de>
2019-05-20 21:46:27 +0200

    add missing import, clang, compile agains 16.1.4

--- android/src/firebase/analytics/TitaniumFirebaseAnalyticsModule.java
+++ android/src/firebase/analytics/TitaniumFirebaseAnalyticsModule.java
@@ -7,115 +7,128 @@
 
 package firebase.analytics;
 
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import java.io.IOException;
+import java.util.Map;
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.kroll.common.Log;
 import org.appcelerator.kroll.common.TiConfig;
-import org.appcelerator.kroll.KrollDict;
-import org.appcelerator.kroll.KrollModule;
-
-import org.appcelerator.titanium.io.TiBaseFile;
+import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.titanium.TiBlob;
+import org.appcelerator.titanium.io.TiBaseFile;
 import org.appcelerator.titanium.util.TiConvert;
 
-import android.os.Bundle;
-import android.app.Activity;
-
-import com.google.firebase.analytics.FirebaseAnalytics;
-
-import java.io.IOException;
-import java.util.Map;
-
-@Kroll.module(name="TitaniumFirebaseAnalytics", id="firebase.analytics")
+@Kroll.module(name = "TitaniumFirebaseAnalytics", id = "firebase.analytics")
 public class TitaniumFirebaseAnalyticsModule extends KrollModule
 {
-  private static final String LCAT = "TitaniumFirebaseAnalyticsModule";
-  private static final boolean DBG = TiConfig.LOGD;
-  private static FirebaseAnalytics mFirebaseAnalytics;
+	private static final String LCAT = "TitaniumFirebaseAnalyticsModule";
+	private static final boolean DBG = TiConfig.LOGD;
+	private static FirebaseAnalytics mFirebaseAnalytics;
 
-  private FirebaseAnalytics analyticsInstance()
-  {
-    if (mFirebaseAnalytics == null) {
-      mFirebaseAnalytics = FirebaseAnalytics.getInstance(getActivity().getApplicationContext());
-    }
+	private FirebaseAnalytics analyticsInstance()
+	{
+		if (mFirebaseAnalytics == null) {
+			mFirebaseAnalytics = FirebaseAnalytics.getInstance(getActivity().getApplicationContext());
+		}
 
-    return mFirebaseAnalytics;
-  }
+		return mFirebaseAnalytics;
+	}
 
-  @Kroll.method
-  public void log(String name, @Kroll.argument(optional = true) KrollDict parameters)
-  {
-    this.analyticsInstance().logEvent(name, this.mapToBundle(parameters));
-  }
+	@Kroll.method
+	public void log(String name, @Kroll.argument(optional = true) KrollDict parameters)
+	{
+		this.analyticsInstance().logEvent(name, this.mapToBundle(parameters));
+	}
 
-  @Kroll.method
-  public void resetAnalyticsData()
-  {
-    // TODO: Uncomment once Ti.PlayServices >= 11.6.0 is used
-    // this.analyticsInstance().resetAnalyticsData();
-    Log.e("FirebaseAnalytics", "The \"resetAnalyticsData()\" method on Android requires Ti.PlayServices >= 11.6.0");
-  }
+	@Kroll.method
+	public void resetAnalyticsData()
+	{
+		// TODO: Uncomment once Ti.PlayServices >= 11.6.0 is used
+		// this.analyticsInstance().resetAnalyticsData();
+		Log.e("FirebaseAnalytics", "The \"resetAnalyticsData()\" method on Android requires Ti.PlayServices >= 11.6.0");
+	}
 
-  @Kroll.method @Kroll.setProperty
-  public void setEnabled(Boolean enabled)
-  {
-    this.analyticsInstance().setAnalyticsCollectionEnabled(enabled);
-  }
+	// clang-format off
+	@Kroll.method
+	@Kroll.setProperty
+	public void setEnabled(Boolean enabled)
+	// clang-format on
+	{
+		this.analyticsInstance().setAnalyticsCollectionEnabled(enabled);
+	}
 
-  @Kroll.method @Kroll.setProperty
-  public void setUserPropertyString(KrollDict parameters)
-  {
-    this.analyticsInstance().setUserProperty(TiConvert.toString(parameters, "name"), TiConvert.toString(parameters, "value"));
-  }
+	// clang-format off
+	@Kroll.method
+	@Kroll.setProperty
+	public void setUserPropertyString(KrollDict parameters)
+	// clang-format on
+	{
+		this.analyticsInstance().setUserProperty(TiConvert.toString(parameters, "name"),
+												 TiConvert.toString(parameters, "value"));
+	}
 
-  @Kroll.method @Kroll.setProperty
-  public void setUserID(String id)
-  {
-    this.analyticsInstance().setUserId(id);
-  }
+	// clang-format off
+	@Kroll.method
+	@Kroll.setProperty
+	public void setUserID(String id)
+	// clang-format on
+	{
+		this.analyticsInstance().setUserId(id);
+	}
 
-  @Kroll.method @Kroll.setProperty
-  public void setScreenNameAndScreenClass(KrollDict parameters)
-  {
-    if (!parameters.containsKey("screenName")) {
-      Log.e(LCAT, "Unable to set current screen without the missing \"screenName\" key");
-      return;
-    }
+	// clang-format off
+	@Kroll.method
+	@Kroll.setProperty
+	public void setScreenNameAndScreenClass(KrollDict parameters)
+	// clang-format on
+	{
+		if (!parameters.containsKey("screenName")) {
+			Log.e(LCAT, "Unable to set current screen without the missing \"screenName\" key");
+			return;
+		}
 
-    final String screenName = parameters.getString("screenName");
-    final String screenClass = parameters.optString("screenClass", "TiController");
-    final FirebaseAnalytics instance = analyticsInstance();
+		final String screenName = parameters.getString("screenName");
+		final String screenClass = parameters.optString("screenClass", "TiController");
+		final FirebaseAnalytics instance = analyticsInstance();
 
-    getActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        instance.setCurrentScreen(TiApplication.getInstance().getCurrentActivity(), screenName, screenClass);
-      }
-    });
-  }
+		getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run()
+			{
+				instance.setCurrentScreen(TiApplication.getInstance().getCurrentActivity(), screenName, screenClass);
+			}
+		});
+	}
 
-  private static Bundle mapToBundle(Map<String, Object> map)
-  {
-    if (map == null || map.size() == 0) return null;
+	private static Bundle mapToBundle(Map<String, Object> map)
+	{
+		if (map == null || map.size() == 0)
+			return null;
 
-    Bundle bundle = new Bundle(map.size());
+		Bundle bundle = new Bundle(map.size());
 
-    for (String key : map.keySet()) {
-      Object val = map.get(key);
-      if (val == null) {
-        bundle.putString(key, null);
-      } else if (val instanceof TiBlob) {
-        bundle.putByteArray(key, ((TiBlob)val).getBytes());
-      } else if (val instanceof TiBaseFile) {
-        try {
-          bundle.putByteArray(key, ((TiBaseFile)val).read().getBytes());
-        } catch (IOException e) {
-          Log.e("FirebaseAnalytics", "Unable to put '" + key + "' value into bundle: " + e.getLocalizedMessage(), e);
-        }
-      } else {
-        bundle.putString(key, TiConvert.toString(val));
-      }
-    }
+		for (String key : map.keySet()) {
+			Object val = map.get(key);
+			if (val == null) {
+				bundle.putString(key, null);
+			} else if (val instanceof TiBlob) {
+				bundle.putByteArray(key, ((TiBlob) val).getBytes());
+			} else if (val instanceof TiBaseFile) {
+				try {
+					bundle.putByteArray(key, ((TiBaseFile) val).read().getBytes());
+				} catch (IOException e) {
+					Log.e("FirebaseAnalytics",
+						  "Unable to put '" + key + "' value into bundle: " + e.getLocalizedMessage(), e);
+				}
+			} else {
+				bundle.putString(key, TiConvert.toString(val));
+			}
+		}
 
-    return bundle;
-  }
+		return bundle;
+	}
 }
